929a9f2141bb9dcb5ece63efb18b6778d3915dc2
Victor Lin <bornstub@gmail.com>
2013-10-04 17:16:08 +0800

    Fix postgresql server deployment issue

--- cookbooks/postgresql/recipes/server.rb
+++ cookbooks/postgresql/recipes/server.rb
@@ -48,19 +48,13 @@ else
   node.save
 end
 
-directory "#{node['postgresql']['dir']}" do
-  mode 00777
-  owner "postgres"
-  group "postgres"
-  action :create
-  recursive true
-end
-directory "/var/log/postgresql" do
-  mode 00777
-  owner "postgres"
-  group "postgres"
-  action :create
-  recursive true
+# Include the right "family" recipe for installing the server
+# since they do things slightly differently.
+case node['platform_family']
+when "rhel", "fedora", "suse"
+  include_recipe "postgresql::server_redhat"
+when "debian"
+  include_recipe "postgresql::server_debian"
 end
 
 template "#{node['postgresql']['dir']}/postgresql.conf" do
@@ -77,15 +71,6 @@ template "#{node['postgresql']['dir']}/pg_hba.conf" do
   group "postgres"
   mode 00600
   notifies :reload, 'service[postgresql]', :immediately
-end
-
-# Include the right "family" recipe for installing the server
-# since they do things slightly differently.
-case node['platform_family']
-when "rhel", "fedora", "suse"
-  include_recipe "postgresql::server_redhat"
-when "debian"
-  include_recipe "postgresql::server_debian"
 end
 
 # NOTE: Consider two facts before modifying "assign-postgres-password":
